98eab6a6a28e5925fb7ca43de910cfa949ae64c0
Param Singh <paramsingh258@gmail.com>
2014-11-28 03:19:22 +0530

    Configure templates and static files.

--- rango/views.py
+++ rango/views.py
@@ -1,9 +1,15 @@
-#from django.shortcuts import render
-from django.http import HttpResponse
- 
-def index(request):
-	return HttpResponse("Rango says, \"Hello, World\"\
-		<br/><a href = '/rango/about/'> About </a>")
+# Commented out because of being there in the default config but not being
+# used.
+from django.shortcuts import render
 
+# django.template import render
+from django.http import HttpResponse
+
+# Index page 
+def index(request):
+	context_dict = {'boldmessage':'I am bold from the context'}
+	return render(request, 'rango/index.html', context_dict)
+
+# About page
 def about(request):
 	return HttpResponse("Hello E-410")
